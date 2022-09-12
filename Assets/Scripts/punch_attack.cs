160cd9f4ac3de9c051f9a7ac755f91174ff6014f
William Rockwell <44515502+Ralhm@users.noreply.github.com>
2022-09-12 11:55:08 -0600

    updated my branch

--- Assets/Scripts/punch_attack.cs
+++ Assets/Scripts/punch_attack.cs
@@ -8,26 +8,18 @@ public class punch_attack : MonoBehaviour
 {
     public TMP_Text score_text;
     public GameObject blood;
-<<<<<<< HEAD
-    public int score=0;
-=======
     public Transform flag;
->>>>>>> main
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
-        score_text.text = score.ToString();
-=======
         score_text.text = scene_save.score.ToString();
->>>>>>> main
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -39,21 +31,16 @@ public class punch_attack : MonoBehaviour
         }
     }
 
-    IEnumerator enemyDead(Collider2D other) {
+    IEnumerator enemyDead(Collider2D other)
+    {
 
         if (other.gameObject.GetComponent<Enemy>())
         {
             other.gameObject.GetComponent<Enemy>().e_animator.SetBool("dead", true);
-<<<<<<< HEAD
-            score += 100;
-            yield return new WaitForSeconds(1f);
-
-=======
             scene_save.score += 100;
             yield return new WaitForSeconds(0.5f);
->>>>>>> main
 
-           
+
             other.gameObject.GetComponent<Enemy>().dead();
 
 
@@ -62,32 +49,27 @@ public class punch_attack : MonoBehaviour
                       other.gameObject.transform.position,
                       other.gameObject.transform.rotation);
         }
-        if(other.gameObject.GetComponent<Guest>()) {
-            other.gameObject.GetComponent<Guest>().g_animator.SetBool("dead",true);
+        if (other.gameObject.GetComponent<Guest>())
+        {
+            other.gameObject.GetComponent<Guest>().g_animator.SetBool("dead", true);
             if (other.gameObject.GetComponent<Guest>().isTarget)
             {
-<<<<<<< HEAD
-                score += 1000;
-            }
-            else {
-                score -= 500;
-=======
                 flag.localPosition =
-                  new Vector3(other.gameObject.transform.position.x, 
+                  new Vector3(other.gameObject.transform.position.x,
                   other.gameObject.transform.position.y, 0);
                 scene_save.score += 1000;
             }
-            else {
+            else
+            {
                 scene_save.score -= 500;
->>>>>>> main
             }
-            
+
             yield return new WaitForSeconds(0.8f);
-            
+
 
             other.gameObject.GetComponent<Guest>().dead();
         }
-      
+
     }
 
 
