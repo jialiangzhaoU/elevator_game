44cf12f2e3c2ab23cdfea620feaa1a68a9b3f0c5
jialiangzhaoU <u1409860@umail.utah.edu>
2022-09-11 20:59:29 -0600

    zhao_9/11_20:58
    finish guest animation and code, finish score system

--- Assets/Scripts/punch_attack.cs
+++ Assets/Scripts/punch_attack.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class punch_attack : MonoBehaviour
 {
-
+    public TMP_Text score_text;
     public GameObject blood;
+    public int score=0;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +19,7 @@ public class punch_attack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        score_text.text = score.ToString();
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -33,6 +36,7 @@ public class punch_attack : MonoBehaviour
         if (other.gameObject.GetComponent<Enemy>())
         {
             other.gameObject.GetComponent<Enemy>().e_animator.SetBool("dead", true);
+            score += 100;
             yield return new WaitForSeconds(1f);
 
 
@@ -46,7 +50,13 @@ public class punch_attack : MonoBehaviour
         }
         if(other.gameObject.GetComponent<Guest>()) {
             other.gameObject.GetComponent<Guest>().g_animator.SetBool("dead",true);
-
+            if (other.gameObject.GetComponent<Guest>().isTarget)
+            {
+                score += 1000;
+            }
+            else {
+                score -= 500;
+            }
             
             yield return new WaitForSeconds(0.8f);
             
